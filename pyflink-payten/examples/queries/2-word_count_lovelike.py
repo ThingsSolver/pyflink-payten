adf8cfcaff66725b4a53979e89d5bc02035f7ea7
vikalfc <violeta.milanovic10@gmail.com>
2021-06-22 17:24:58 +0200

    generators pep8

--- pyflink-payten/examples/queries/2-word_count_lovelike.py
+++ pyflink-payten/examples/queries/2-word_count_lovelike.py
@@ -30,9 +30,11 @@ t_env.connect(
 )
 
 t_env.connect(
-    FileSystem().path("/opt/examples/data/output/2_word_count_lovelike_output.csv")
+    FileSystem()
+    .path("/opt/examples/data/output/2_word_count_lovelike_output.csv")
 ).with_format(Csv().derive_schema()).with_schema(
-    Schema().field("reviewText", DataTypes.STRING()).field("counts", DataTypes.INT())
+    Schema().field("reviewText", DataTypes.STRING())
+    .field("counts", DataTypes.INT())
 ).create_temporary_table(
     "mySink"
 )
@@ -40,10 +42,10 @@ t_env.connect(
 # query
 final_table = t_env.sql_query(
     """
-    SELECT 
+    SELECT
     reviewText,
     (CHAR_LENGTH(reviewText)-
-     CHAR_LENGTH(REPLACE(reviewText, 'like', ''))) / char_length('like') + 
+     CHAR_LENGTH(REPLACE(reviewText, 'like', ''))) / char_length('like') +
     (CHAR_LENGTH(reviewText)-
     CHAR_LENGTH(REPLACE(reviewText, 'love', ''))) / char_length('love')
     FROM mySource
